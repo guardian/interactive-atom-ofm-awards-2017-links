5737e14a6eded99c7d168efebaae74966406e838
Duarte Carrilho da Graça <duarte.carrilho.casual@theguardian.com>
2017-10-11 11:46:52 +0100

    app css fixes

--- src/js/main.js
+++ src/js/main.js
@@ -9,26 +9,43 @@ document.body.appendChild(el);
 
   // clone css
   var style = document.querySelector('style:last-of-type');
-  doc.querySelector('body').appendChild(style);
+  if (style) {
+    doc.querySelector('body').appendChild(style);
+  }
 
   var nextPrevAwards = findNextPrev();
   if (nextPrevAwards) {
 
+    console.log(document);
+    console.log(document.querySelectorAll('body'));
+
     var oNav = document.querySelector('.series-nav');
 
     // clone
-    var topNav = cloneWithClass(oNav, 'top');
+    var topNavDesktop = cloneWithClass(oNav, 'top');
+    var topNavMobile = cloneWithClass(oNav, 'top');
     var bottomNav = cloneWithClass(oNav, 'bottom');
 
-    var topNav = updateContentWith(topNav, nextPrevAwards);
+    var topNavDesktop = updateContentWith(topNavDesktop, nextPrevAwards);
+    var topNavMobile = updateContentWith(topNavMobile, nextPrevAwards);
     var bottomNav = updateContentWith(bottomNav, nextPrevAwards);
 
     // inject
-    var curtains = doc.querySelector('.l-side-margins #article');
-    curtains.parentElement.insertBefore(topNav, curtains);
+    var desktopHeader = doc.querySelector('#article .hide-on-mobile header .tonal__standfirst');
+    if (desktopHeader) {
+      desktopHeader.appendChild(topNavDesktop);
+    }
 
-    var articleText = doc.querySelector('.content__article-body');
-    articleText.appendChild(bottomNav);
+    var mobileHeader = doc.querySelector('.mobile-only header .tonal__standfirst, .article .article__header');
+    if (mobileHeader) {
+      mobileHeader.appendChild(topNavMobile);
+    }
+
+
+    var articleText = doc.querySelector('.content__article-body, .article__body > div');
+    if (articleText) {
+      articleText.appendChild(bottomNav);
+    }
   }
 
   // cleanup
@@ -36,9 +53,9 @@ document.body.appendChild(el);
 
   function updateContentWith(n, nextPrevAwards) {
     n.querySelector('.next').innerText = nextPrevAwards[1][0];
-    n.querySelector('.next').setAttribute('href', nextPrevAwards[1][1][0]);
+    n.querySelector('.next').setAttribute('href', '/'+nextPrevAwards[1][1][0]);
     n.querySelector('.prev').innerText = nextPrevAwards[0][0];
-    n.querySelector('.prev').setAttribute('href', nextPrevAwards[0][1][0]);
+    n.querySelector('.prev').setAttribute('href', '/'+nextPrevAwards[0][1][0]);
     return n;
   }
 
@@ -50,23 +67,23 @@ document.body.appendChild(el);
 
   function findNextPrev() {
     var allAwards = [
-      ['Best restaurant', ['#replace/this/later', 'global/2016/nov/25/my-atom-idea']],
-      ['Best cheap eats', ['#replace/this/later']],
-      ['Best Sunday lunch', ['#replace/this/later']],
-      ['Best ethical food project', ['#replace/this/later', '#replace/this/later/also']],
-      ['Best food personality', ['#replace/this/later']],
-      ['Best place to drink', ['#replace/this/later']],
-      ['Best new cookbook', ['#replace/this/later']],
-      ['Best independent retailer', ['#replace/this/later']],
-      ['Best Instagram feed for food lovers', ['#replace/this/later']],
-      ['OFM local food hero', ['#replace/this/later']],
-      ['Best reader’s recipe', ['#replace/this/later']],
-      ['Young chef of the year', ['#replace/this/later']],
-      ['Best newcomer in food and drink', ['#replace/this/later']],
-      ['Best producer', ['#replace/this/later']],
-      ['Outstanding achievement', ['#replace/this/later']],
-      ['Lifetime achievement', ['#replace/this/later']],
-      ['Editor’s Award', ['#replace/this/later']]
+      ['Best restaurant', ['lifeandstyle/2017/oct/15/ofm-awards-2017-best-restaurant-pidgin', 'lifeandstyle/2017/oct/15/ofm-awards-2017-best-restaurant-runners-up']],
+      ['Best cheap eats', ['lifeandstyle/2017/oct/15/ofm-awards-2017-best-cheap-eats-yard-sale-pizza-london', 'lifeandstyle/2017/oct/15/ofm-awards-2017-best-cheap-eats-runners-up']],
+      ['Best Sunday lunch', ['lifeandstyle/2017/oct/15/ofm-awards-2017-best-sunday-lunch-a-rule-of-tum-bookshop', 'lifeandstyle/2017/oct/15/ofm-awards-2017-best-sunday-lunch-the-runners-up']],
+      ['Best ethical food project', ['lifeandstyle/2017/oct/15/cook-for-syria-ofm-awards-2017-best-ethical-project', 'lifeandstyle/2017/oct/15/ofm-awards-2017-best-ethical-project-runners-up']],
+      ['Best food personality', ['lifeandstyle/2017/oct/15/ofm-awards-2017-best-food-personality-rick-stein']],
+      ['Best place to drink', ['lifeandstyle/2017/oct/15/2017-best-place-to-drink-crown-hastings-ofm-awards', 'lifeandstyle/2017/oct/15/ofm-awards-2017-best-places-to-drink-runners-up-bars-pubs-cocktails']],
+      ['Best new cookbook', ['lifeandstyle/2017/oct/15/ofm-awards-2017-best-new-cookbook-fresh-india-meera-sodha']],
+      ['Best independent retailer', ['lifeandstyle/2017/oct/15/ofm-awards-2017-best-independent-retailer-rafis-spicebox', 'lifeandstyle/2017/oct/15/ofm-awards-2017-best-independent-retailer-the-runners-up']],
+      // ['Best Instagram feed for food lovers', ['lifeandstyle/gallery/2017/oct/15/ofm-awards-2017-best-instagram-feed-for-food-lovers-jamie-oliver']],
+      ['Local food hero', ['lifeandstyle/2017/oct/15/ofm-awards-2017-local-food-hero-pop-up-soup-kitchen-trevor-blaney']],
+      ['Best reader’s recipe', ['lifeandstyle/2017/oct/15/ofm-awards-2017-best-readers-recipe-maryam-sham-bakhlava-traybake']],
+      ['Young chef of the year', ['lifeandstyle/2017/oct/15/ofm-awards-2017-young-chef-of-the-year-tom-adams']],
+      ['Best newcomer', ['lifeandstyle/2017/oct/15/ofm-awards-2017-best-newcomer-temper-soho']],
+      ['Best producer', ['lifeandstyle/2017/oct/15/ofm-awards-2017-best-producer-bisous-bisous']],
+      ['Outstanding achievement', ['lifeandstyle/2017/oct/15/ofm-awards-2017-outstanding-achievement-trussell-trust-food-banks']],
+      ['Lifetime achievement', ['lifeandstyle/2017/oct/15/ofm-awards-2017-lifetime-achievement-joyce-molyneux']],
+      ['Editor’s Award', ['lifeandstyle/2017/oct/15/ofm-awards-2017-editors-award-aa-gill']]
     ];
 
     if (window.location.protocol === 'file:') {
@@ -92,7 +109,4 @@ document.body.appendChild(el);
 
   }
 
-  function injectBefore() {
-
-  }
 })();
